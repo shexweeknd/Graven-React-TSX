b564a3b1d129235e43399adf89a94df6005b9b37
shexweeknd <ramarosandratananantenaina@gmail.com>
2023-03-11 17:52:42 +0300

    un petit edit sur les SocialLinks

---
+++ my-app/src/components/SocialLinks.tsx
@@ -0,0 +1,12 @@
+export const SocialLinks = () => {
+    return (
+        <section>
+            <h3>Réseaux Sociaux</h3>
+            <ul>
+                <li><a href="https://www.facebook.com">Facebook</a></li>
+                <li><a href="https://www.linkedin.com">Linkedin</a></li>
+                <li><a href="https://www.twitter.com">Twitter</a></li>
+            </ul>
+        </section>
+    )
+}
--- my-app/src/screens/Home.tsx
+++ my-app/src/screens/Home.tsx
@@ -1,12 +1,13 @@
 import { About } from "../components/About"
 import { YoutubeVideo } from "../components/YoutubeVideo"
+import { SocialLinks } from "../components/SocialLinks"
 
 export const Home = () => {
     return (
         <div>
             <About />
-            <YoutubeVideo id="bU7Ue4xF3YI"/>
-            <YoutubeVideo id="rffvetYDlhw"/>
+            <YoutubeVideo id="bU7Ue4xF3YI" />
+            <SocialLinks />
         </div>
     )
 }
