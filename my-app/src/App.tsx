841b70c16e3addaed8ed76d730cf4c105ddc524c
shexweeknd <ramarosandratananantenaina@gmail.com>
2023-03-12 12:26:45 +0300

    2e partie ajout de resource screen et des routes
    j'ai installé react-router-dom@6 et j'ai inscrit les routes dans index.tsx et App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './screens/Home';
+import { Ressources } from './screens/Ressources';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ressources" element={<Ressources />} />
+      </Routes>
     </div>
   );
 }
---
+++ my-app/src/screens/Ressources.tsx
@@ -0,0 +1,13 @@
+export const Ressources = () => {
+    return (
+        <div>
+            <h3>Ressources de shexweeknd</h3>
+
+            <ul>
+                <li><a href="https://www.python.org">Télécharger python</a></li>
+                <li><a href="https://www.github.com/shexweeknd">Les projets de Shexweeknd</a></li>
+                <li><a href="https://www.pypi.org">Télécharger des paquets Python</a></li>
+            </ul>
+        </div>
+    )
+}
