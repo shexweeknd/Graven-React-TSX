62530937f28e3621ba9561fb6440fbb444d01920
shexweeknd <ramarosandratananantenaina@gmail.com>
2023-03-13 09:51:47 +0300

    recupération des données du formulaire
    recuperation des données du form en utilisant react-hook-form

--- my-app/src/screens/Contacts.tsx
+++ my-app/src/screens/Contacts.tsx
@@ -1,24 +1,39 @@
+import { useForm, SubmitHandler } from "react-hook-form"
+
+type FormInputs = {
+    subject: string,
+    email: string,
+    message: string
+}
+
 export const Contacts = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm<FormInputs>()
+    const onSubmit: SubmitHandler<FormInputs> = data => {
+        console.log(data)
+    }
+
     return (
         <main>
             <h3 className="font-extrabold mb-4 text-3xl">Contacts</h3>
 
             <p>EMAIL: ShexWeeknd@gmail.com</p>
 
-            <form className="mt-5" action="">
+            <form className="mt-5" onSubmit={handleSubmit(onSubmit)}>
                 <div>
                     <label className="block" htmlFor="subject">Sujet du mail</label>
-                    <input className="border" type="text" name="subject" id="subject" placeholder="Quel est le sujet" />
+                    <input {...register("subject")} className="border" type="text" name="subject" id="subject" placeholder="Quel est le sujet" />
                 </div>
 
                 <div>
                     <label className="block" htmlFor="email">Email</label>
-                    <input className="border" type="email" name="email" id="email" placeholder="votre email" />
+                    <input {...register("email"), { required: true }} className="border" type="email" name="email" id="email" placeholder="votre email" />
                 </div>
+                    {errors.email && <p className="texct-[red]">Email requis</p>}
 
                 <div>
                     <label className="block" htmlFor="message">Sujet du mail</label>
-                    <textarea rows={5} className="border resize-none" name="message" id="message" placeholder="Inscrivez votre message ici" />
+                    <textarea {...register("message")} rows={5} className="border resize-none" name="message" id="message" placeholder="Inscrivez votre message ici" />
                 </div>
 
                 <input type="submit" className="block bg-[purple] text-[white] py-3 px-3 hover:bg-[gray]" />
